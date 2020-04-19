d2e5221593438f108700b470f497c81cc1dd7c9b
Ed Williams <ed@madebyew.com>
2020-04-19 13:13:53 +0100

    first commit

---
+++ precache-manifest.4f33a3cfefbfde9fe0c8873dab6d4920.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b550831d5c61fc746335ca329fa7e7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "477bb47b8876732cfb61",
+    "url": "/static/css/main.886ff295.chunk.css"
+  },
+  {
+    "revision": "cf1ff00b265125a1d184",
+    "url": "/static/js/2.9b3c8058.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9b3c8058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "477bb47b8876732cfb61",
+    "url": "/static/js/main.a4003271.chunk.js"
+  },
+  {
+    "revision": "3ce4e42e7804c009b975",
+    "url": "/static/js/runtime-main.8bff3599.js"
+  },
+  {
+    "revision": "45ab49d3b5a729619389a18af597b5e0",
+    "url": "/static/media/colour-heist-logo.45ab49d3.svg"
+  }
+]);
