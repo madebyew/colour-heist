54caf25844a1a0537414a5619629849505ca455a
Ed Williams <ed@madebyew.com>
2020-04-19 14:34:58 +0100

    Build.

---
+++ precache-manifest.624c7a97efffd66b53452a2b9dd8f8b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64043482b84ce7cb7bce3b299335ac6",
+    "url": "/colour-heist/index.html"
+  },
+  {
+    "revision": "3b4055bee288daecd2ae",
+    "url": "/colour-heist/static/css/main.886ff295.chunk.css"
+  },
+  {
+    "revision": "cf1ff00b265125a1d184",
+    "url": "/colour-heist/static/js/2.9b3c8058.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/colour-heist/static/js/2.9b3c8058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4055bee288daecd2ae",
+    "url": "/colour-heist/static/js/main.d7ca8b0d.chunk.js"
+  },
+  {
+    "revision": "2cc1f83c13fd3742d3b4",
+    "url": "/colour-heist/static/js/runtime-main.25eb4682.js"
+  },
+  {
+    "revision": "45ab49d3b5a729619389a18af597b5e0",
+    "url": "/colour-heist/static/media/colour-heist-logo.45ab49d3.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f33a3cfefbfde9fe0c8873dab6d4920.js"
+  "/colour-heist/precache-manifest.624c7a97efffd66b53452a2b9dd8f8b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/colour-heist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
